f23372559715510981279eeafe4fbfe13e1ed428
Philip von Koss <phpmvk@gmail.com>
2023-04-15 19:22:23 +0200

    feat: added method to get listing from client store by id

--- client/rental-genie-app/src/app/client-store.ts
+++ client/rental-genie-app/src/app/client-store.ts
@@ -5,12 +5,16 @@ import { Injectable } from "@angular/core";
 })
 export class Store {
 
-  private allListings: [] = []
+  private allListings: any[] = []
 
   constructor(){}
 
   getListing(){
     return this.allListings
+  }
+
+  getListingById(id: string) {
+    return this.allListings.find(el => el._id = id)
   }
 
   setListings(listings: []){
