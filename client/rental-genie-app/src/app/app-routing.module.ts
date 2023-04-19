17ac2187133767afa339f4f04a4d990adeee549b
Philip von Koss <phpmvk@gmail.com>
2023-04-19 16:32:11 +0200

    feat: set up initial structure for route guards to protect private side

--- client/rental-genie-app/src/app/app-routing.module.ts
+++ client/rental-genie-app/src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'public', pathMatch: 'full' },
   { path: 'public', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: 'private', loadChildren: () => import('./private/private.module').then(m => m.PrivateModule) },
+  { path: 'private', loadChildren: () => import('./private/private.module').then(m => m.PrivateModule), canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'public' } // handle all other routes, redirect to public
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
