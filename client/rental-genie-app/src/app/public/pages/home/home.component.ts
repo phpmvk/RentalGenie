18bbd6c563337fc50bfbb41924e9aafa768802d2
Philip von Koss <phpmvk@gmail.com>
2023-04-22 20:24:56 +0200

    feat: can sort listings by price, and dropdowns show available cities and districts. Still need to implement filter for dropdown selectors

--- client/rental-genie-app/src/app/public/pages/home/home.component.ts
+++ client/rental-genie-app/src/app/public/pages/home/home.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiClientService } from 'src/app/api-client.service';
 import { Store } from 'src/app/client-store';
+import { PublicListing } from 'src/app/types';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit{
 
   // fix types!!
-  allListings: any = []
+  allListings: PublicListing[] = []
+  allCities: string[] = [];
+  allDistricts: string[] = [];
+
+  priceSortedByAscending: Boolean = false;
 
   constructor(
     private api: ApiClientService,
@@ -29,8 +34,25 @@ export class HomeComponent implements OnInit{
       this.api.getAllListings().subscribe(res => {
         this.store.setListings(res)
         this.allListings = res;
+        for (let listing of this.allListings) {
+          if (!this.allCities.includes(listing.city)) {
+            this.allCities.push(listing.city)
+          }
+          if (!this.allDistricts.includes(listing.district)) {
+            this.allDistricts.push(listing.district)
+          }
+        }
       })
+    }
+  }
 
+  sortByPrice(){
+    if (this.priceSortedByAscending) {
+      this.allListings.sort((a,b) => a.rent_amount - b.rent_amount)
+      this.priceSortedByAscending = !this.priceSortedByAscending
+    } else {
+      this.allListings.sort((a,b) => b.rent_amount - a.rent_amount)
+      this.priceSortedByAscending = !this.priceSortedByAscending
     }
   }
 }
